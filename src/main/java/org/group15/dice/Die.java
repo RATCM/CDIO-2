0ba30afe08c38c3e92a33164d738f41a6b0349ac
ih8mondays <bjoerkobel@hotmail.dk>
2024-10-29 16:12:35 +0100

    Implemented Die class

--- src/main/java/org/group15/dice/Die.java
+++ src/main/java/org/group15/dice/Die.java
@@ -2,4 +2,22 @@ package org.group15.dice;
 
 public class Die {
 
+    private int value;
+    private int sides;
+    private final java.util.Random random;
+
+    Die(int sides){
+        this.sides = sides;
+        this.value = -1;
+        random = new java.util.Random();
+    }
+
+    void roll() {
+        this.value = random.nextInt(1,(sides+1));
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
 }
