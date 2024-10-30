efd65082421f8b9369375f9554cacb23b43d00c1
TALind <tlindestroem@icloud.com>
2024-10-30 15:38:01 +0100

    Added Account class

---
+++ src/main/java/org/group15/player/Account.java
@@ -0,0 +1,36 @@
+package org.group15.player;
+
+public class Account {
+    private int amount;
+    private final int maxAmount = 3000;
+
+    public Account(int initAmount) {
+        this.amount = Math.max(initAmount, 0);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean addAmount(int amountToAdd) {
+        if (amount >= maxAmount && amountToAdd > 0 ) {
+            return false;
+        }
+        if (amountToAdd > 0 && amount + amountToAdd <= maxAmount) {
+            amount += amountToAdd;
+            return true;
+        } else if (amountToAdd > 0) {
+            amount = maxAmount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeAmount(int amountToRemove) {
+        if(amountToRemove > 0 && amountToRemove <= amount) {
+            amount -= amountToRemove;
+            return true;
+        }
+        return false;
+    }
+}
