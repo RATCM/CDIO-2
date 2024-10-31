7ae5f894913e0e8fdb09c3d7c0afe2cbdbb6c456
Christian Roar Weber <94703124+Baktwoone@users.noreply.github.com>
2024-10-31 14:07:33 +0100

    changes to 'ExtraTurnEffect'

--- src/main/java/org/group15/effects/ExtraTurnEffect.java
+++ src/main/java/org/group15/effects/ExtraTurnEffect.java
@@ -4,10 +4,11 @@ import org.group15.game.context.GameContext;
 import org.group15.player.Player;
 
 
-// Needs fixing
 public class ExtraTurnEffect extends Effect {
+    protected GameContext context;
 
     public ExtraTurnEffect(GameContext context) {
+        super(context);
         this.context = context;
     }
 
@@ -15,8 +16,7 @@ public class ExtraTurnEffect extends Effect {
         ExtraTurnState.doNextTurn(this.context);
     }
     public String getDescription() {
-        Player thisPlayer = this.context.getCurrentPlayer();
-        return thisPlayer.getName() + " gets another turn!";
+        return this.context.getCurrentPlayer().getName() + " gets another turn!";
     }
     public Alignment getAlignment() {
         return Alignment.Good;
