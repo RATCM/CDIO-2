112d4f7d49b3fdfd5155b429fc87214b897c4afd
Oliver-hvit <s246566@dtu.dk>
2024-10-31 14:09:40 +0100

    Implemented PointChangeEffect class

--- src/main/java/org/group15/effects/PointChangeEffect.java
+++ src/main/java/org/group15/effects/PointChangeEffect.java
@@ -1,5 +1,42 @@
 package org.group15.effects;
+import org.group15.game.context.GameContext;
 
-public class PointChangeEffect {
-    
+
+public class PointChangeEffect extends Effect{
+    private int amount;
+    protected GameContext context;
+
+    public PointChangeEffect(int amount, GameContext context) {
+        super(context);
+        this.amount = amount;
+        this.context = context;
+    }
+
+    public void apply() {
+        if (amount > 0) {
+            this.context.getCurrentPlayer().getAccount().addAmount(amount);
+        } else {
+            this.context.getCurrentPlayer().getAccount().removeAmount(-amount);
+        }
+    }
+
+    public String getDescription() {
+        if (amount > 0) {
+            return "Adds " + amount + " points to player " + this.context.getCurrentPlayer() + "'s account";
+        } else if (amount < 0) {
+            return "Removes " + amount + " points from player " + this.context.getCurrentPlayer() + "'s account";
+        } else {
+            return "no points added or removed";
+        }
+    }
+
+    public Alignment getAlignment() {
+        if (amount > 0) {
+            return Alignment.Good;
+        } else if (amount == 0) {
+            return Alignment.Neutral;
+        } else {
+            return Alignment.Bad;
+        }
+    } 
 }
