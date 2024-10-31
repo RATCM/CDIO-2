822bd3378bff690f94f50b9abf8e26a7eb0f6d57
ratcm <isakbjoershol@mbptilhendeisak.students.clients.local>
2024-10-31 14:49:04 +0100

    Added SimpleGameView class

---
+++ src/main/java/org/group15/game/play/SimpleGameView.java
@@ -0,0 +1,18 @@
+package org.group15.game.play;
+
+import org.group15.player.Player;
+
+public class SimpleGameView implements GameView {
+
+    // TODO
+    @Override
+    public void showField(Field field) {
+
+    }
+
+    // TODO
+    @Override
+    public void showWinningPlayer(Player player) {
+
+    }
+}
