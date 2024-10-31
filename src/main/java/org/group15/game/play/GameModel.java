4ea62a193ccf6cb5707c5c322f6a59c0585de7af
ratcm <isakbjoershol@mbptilhendeisak.students.clients.local>
2024-10-31 14:47:50 +0100

    Added GameModel class

---
+++ src/main/java/org/group15/game/play/GameModel.java
@@ -0,0 +1,24 @@
+package org.group15.game.play;
+
+import org.group15.game.context.GameContext;
+import org.group15.player.Player;
+public class GameModel {
+    private GameContext gameContext;
+    private Field[] fields;
+
+    // TODO
+    GameModel(Player[] players) {
+
+    }
+
+    // TODO
+    public GameContext getContext() {
+        return null;
+    }
+
+    // TODO
+    public Field[] getFields () {
+        return null;
+    }
+
+}
