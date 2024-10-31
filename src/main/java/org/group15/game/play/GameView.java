55f176250f5fd8d9a60763f045dd8e1373abacbf
ratcm <isakbjoershol@mbptilhendeisak.students.clients.local>
2024-10-31 14:48:35 +0100

    Added GameView interface

---
+++ src/main/java/org/group15/game/play/GameView.java
@@ -0,0 +1,8 @@
+package org.group15.game.play;
+
+import org.group15.player.Player;
+
+public interface GameView {
+    public void showField(Field field);
+    public void showWinningPlayer(Player player);
+}
