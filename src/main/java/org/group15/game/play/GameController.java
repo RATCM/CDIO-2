16d6db6992927c61c0bf95b837551128b117dc42
Andreas <s245053@dtu.dk>
2024-11-01 14:55:00 +0100

    updated GameModel, soulution may not be subject for change

--- src/main/java/org/group15/game/play/GameController.java
+++ src/main/java/org/group15/game/play/GameController.java
@@ -1,8 +1,12 @@
 package org.group15.game.play;
 
 import org.group15.dice.DiceController;
+import org.group15.dice.DiceModel;
 import org.group15.dice.Die;
+import org.group15.game.context.GameContext;
 import org.group15.player.Player;
+
+import java.util.Scanner;
 
 public class GameController {
     private GameView view;
@@ -11,6 +15,9 @@ public class GameController {
 
     // TODO
     public GameController(Player[] player){
+        this.model = new GameModel(player);
+        this.view = new SimpleGameView(new Scanner(System.in));
+        this.diceController = new DiceController(2,6);
 
     }
 
@@ -18,13 +25,22 @@ public class GameController {
     // Used if you wanna use a different GameView instead
     // of the default one
     public GameController(Player[] player, GameView view){
-
+        this.model = new GameModel(player);
+        this.view = view;
+        this.diceController = new DiceController(2,6);
     }
 
     // TODO
     // Calls the getUserRoll method in DiceController
     // And updates the game context
     public void rollDice(){
+        int roll = calculateFieldPosition(diceController.retrieveUserRoll().getDice());
+
+        removePlayerFromField();
+
+        putCurrentPlayerOnField(roll-1);
+
+        applyFieldEffectsToCurrentPlayer(roll-1);
 
     }
 
@@ -32,12 +48,19 @@ public class GameController {
     // Displays the field description to the player
     // through the GameView interface
     public void showField(){
+        view.showField(findFieldWithPlayer(model.getContext().getCurrentPlayer()));
 
     }
 
     // TODO
     // Returns whether some player has won.
     public boolean checkForWinner(){
+        for(Player player: model.getContext().getAllPlayers()){
+            if (player.getAccount().getAmount() == 3000){
+                return true;
+            }
+
+        }
         return false;
     }
 
@@ -45,6 +68,14 @@ public class GameController {
     // Displays the winning player through the
     // GameView interface
     public boolean displayWinningPlayer(){
+        Player winPlayer;
+
+        for(Player player: model.getContext().getAllPlayers()){
+            if (player.getAccount().getAmount() == 3000){
+                winPlayer = player;
+                return true;
+            }
+        }
         return false;
     }
 
@@ -52,13 +83,22 @@ public class GameController {
     // Returns the position corresponding to the dice throw.
     // e.g. [2,5] returns 7
     private int calculateFieldPosition(Die[] dice){
-        return -1;
+        int roll = 0;
+
+        for(int i = 0; i < dice.length; i++){
+            roll += dice[i].getValue();
+        }
+        return roll;
     }
 
     // TODO
     // Puts the currentPlayer in the GameContext to the field
     // at a the specified position.
     private boolean putCurrentPlayerOnField(int position){
+         Field field = model.getFields()[position];
+
+         field.addPlayer(model.getContext().getCurrentPlayer());
+
         return false;
     }
 
@@ -66,18 +106,26 @@ public class GameController {
     // Removes the currentPlayer in the GameContext from the field
     // at a the specified position.
     private boolean removePlayerFromField(){
+        for(Field field : model.getFields()){
+            if(field.hasPlayer(model.getContext().getCurrentPlayer())){
+                field.removePlayer(model.getContext().getCurrentPlayer());
+            }
+        }
+
         return false;
     }
 
     // TODO
     // Applies all the field effects to the player.
-    private void applyFieldEffectsToCurrentPlayer(){
+    private void applyFieldEffectsToCurrentPlayer(int roll){
+        model.getFields()[roll].applyFieldEffects();
 
     }
 
     // TODO
     // Returns the field which contains the specified player.
     private Field findFieldWithPlayer(Player player){
+
         return null;
     }
 }
--- src/main/java/org/group15/game/play/GameModel.java
+++ src/main/java/org/group15/game/play/GameModel.java
@@ -13,37 +13,37 @@ public class GameModel {
         this.gameContext = new GameContext(players);
 
         this.fields[0] = new Field(new Effect[]{new PointChangeEffect(250,gameContext)}, 2);
-        this.fields[0].description = gameContext.getCurrentPlayer().getName() + " Climbed a tall tower and found a tresure Chest" + System.lineSeparator();
+        this.fields[0].description = " Climbed a tall tower and found a tresure Chest" + System.lineSeparator();
 
         this.fields[1] = new Field(new Effect[]{new PointChangeEffect(-100,gameContext)}, 3);
-        this.fields[1].description = gameContext.getCurrentPlayer().getName() + " Fell into a crator" + System.lineSeparator();
+        this.fields[1].description = " Fell into a crator" + System.lineSeparator();
 
         this.fields[2] = new Field(new Effect[]{new PointChangeEffect(100,gameContext)}, 4);
-        this.fields[2].description = gameContext.getCurrentPlayer().getName() + " Went through the palace gates" + System.lineSeparator();
+        this.fields[2].description = " Went through the palace gates" + System.lineSeparator();
 
         this.fields[3] = new Field(new Effect[]{new PointChangeEffect(-20,gameContext)}, 5);
-        this.fields[3].description = gameContext.getCurrentPlayer().getName() + " Got stuck in a cold dessert" + System.lineSeparator();
+        this.fields[3].description = " Got stuck in a cold dessert" + System.lineSeparator();
 
         this.fields[4] = new Field(new Effect[]{new PointChangeEffect(180,gameContext)}, 6);
-        this.fields[4].description = gameContext.getCurrentPlayer().getName() + " Found safety in a Walled of city" + System.lineSeparator();
+        this.fields[4].description = " Found safety in a Walled of city" + System.lineSeparator();
 
         this.fields[5] = new Field(new Effect[]{new PointChangeEffect(0,gameContext)}, 7);
-        this.fields[5].description = gameContext.getCurrentPlayer().getName() + " Found peace in the local monastery" + System.lineSeparator();
+        this.fields[5].description = " Found peace in the local monastery" + System.lineSeparator();
 
         this.fields[6] = new Field(new Effect[]{new PointChangeEffect(-70,gameContext)}, 8);
-        this.fields[6].description = gameContext.getCurrentPlayer().getName() + " Got stuck in a black cave" + System.lineSeparator();
+        this.fields[6].description = " Got stuck in a black cave" + System.lineSeparator();
 
         this.fields[7] = new Field(new Effect[]{new PointChangeEffect(60,gameContext)}, 9);
-        this.fields[7].description = gameContext.getCurrentPlayer().getName() + " Found a small hut in the mountains" + System.lineSeparator();
+        this.fields[7].description = " Found a small hut in the mountains" + System.lineSeparator();
 
         this.fields[8] = new Field(new Effect[]{new PointChangeEffect(-80,gameContext), new ExtraTurnEffect(gameContext)}, 10);
-        this.fields[8].description = gameContext.getCurrentPlayer().getName() + " Encounterd a wall of werewolfs and got bit" + System.lineSeparator();
+        this.fields[8].description = " Encounterd a wall of werewolfs and got bit" + System.lineSeparator();
 
         this.fields[9] = new Field(new Effect[]{new PointChangeEffect(-50,gameContext)}, 11);
-        this.fields[9].description = gameContext.getCurrentPlayer().getName() + " Fell into a deep pit" + System.lineSeparator();
+        this.fields[9].description = " Fell into a deep pit" + System.lineSeparator();
 
         this.fields[10] = new Field(new Effect[]{new PointChangeEffect(650,gameContext)}, 12);
-        this.fields[10].description = gameContext.getCurrentPlayer().getName() + " Dug deep a hit GOLD!!!!!" + System.lineSeparator();
+        this.fields[10].description = " Dug deep a hit GOLD!!!!!" + System.lineSeparator();
 
     }
 
